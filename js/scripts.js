e804088da96344d134f9d3f2b9aebbcece145824
arielgonzalezmesa <arielgonzalezmesa@gmail.com>
2024-12-18 15:58:40 +0100

    Task 1.9 Corrections

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ let pokemonRepository = (function (){
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=950';
     let message = document.querySelector('h2');
     let modalContainer = document.querySelector('#modal-container');
+    // let typeName = '';
+    // let abilitiesName = '';
 
     //Other Functions remain here
 
@@ -62,13 +64,13 @@ let pokemonRepository = (function (){
 
     //Function in charge of the Bootstrap Modal
 
-    function showBootstrapModal(item) {
+    function showModal(item) {
+        console.log('Ariel',item);
         let modalBody = $('.modal-body');
         let modalTitle = $('.modal-title');
         let modalHeader = $('.modal-header');
         modalTitle.empty();
         modalBody.empty();
-        
         //creating element for name in modal content
         let nameElement = $('<h1>' + item.name + '</h1>'); 
         //creating img in modal content
@@ -84,6 +86,7 @@ let pokemonRepository = (function (){
         let typesElement = $('<p>' + 'types : ' + item.types + '</p>');
         //creating element for abilities in modal content
         let abilitiesElement = $('<p>' + 'abilities: ' + item.abilities + '</p>' );
+        
 
         modalTitle.append(nameElement);
         modalBody.append(imageElementFront);
@@ -94,35 +97,49 @@ let pokemonRepository = (function (){
         modalBody.append(abilitiesElement);
     }
 
+    // function obtainingStrings(array) {
+    //     let result = '';
+    //     array.forEach(item => result+=item );
+    //     return result
+    // }
+
       // Hiding the modal
-      function hideModal() {
-        modalContainer.classList.remove('is-visible');
-      }
+    //   function hideModal() {
+    //     modalContainer.classList.remove('is-visible');
+    //   }
     
-      window.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-          hideModal();
-        }
-      });
+    //   window.addEventListener('keydown', (e) => {
+    //     if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    //       hideModal();
+    //     }
+    //   });
     
-      modalContainer.addEventListener('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal container,
-        // We only want to close if the user clicks directly on the overlay
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-      });
+    //   modalContainer.addEventListener('click', (e) => {
+    //     // Since this is also triggered when clicking INSIDE the modal container,
+    //     // We only want to close if the user clicks directly on the overlay
+    //     let target = e.target;
+    //     if (target === modalContainer) {
+    //       hideModal();
+    //     }
+    //   });
 
       // Showing all the details of a pokemon
-    function showDetails(pokemon) {
-        loadDetails(pokemon).then(() =>{
-            hideLoadingMessage();
-            //console.log(pokemon);
-            // showModal(pokemon.name, pokemon.height, pokemon.imageUrlFront);
-            showBootstrapModal(pokemon);
-        })
+    // function showDetails(pokemon) {
+    //     loadDetails(pokemon).then((data) =>{
+    //         hideLoadingMessage();
+    //         //console.log(pokemon);
+    //         // showModal(pokemon.name, pokemon.height, pokemon.imageUrlFront);
+    //         showModal(data);
+    //     })
         
+    // }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(() => {
+            hideLoadingMessage();
+            showModal(pokemon);
+        })
+
     }
 
     function includingToButton(button, pokemon){
@@ -183,9 +200,36 @@ let pokemonRepository = (function (){
         })
     }
 
+
+    // With Async and await and creating a new object
+
+    // async function loadDetails(item){
+    //     let url = item.detailsUrl;
+    //     showLoadingMessage();
+    //     try {
+    //         const response= await fetch(url);
+    //         const resJson = await response.json();
+    //         return {
+    //             name: resJson.name,
+    //             imageUrlFront:resJson.sprites.front_default,
+    //             imageUrlFront : resJson.sprites.front_default,
+    //             imageUrlBack : resJson.sprites.back_default,
+    //             height : resJson.height,
+    //             weight : resJson.weight,
+    //             types : parseTypes(resJson.types),
+    //             abilities : parseAbilities(resJson.abilities)
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    //     finally{
+    //         hideLoadingMessage();
+    //     }
+    // }
+
+    //Following Career Foundry's Method
     function loadDetails(item){
         let url = item.detailsUrl;
-        showLoadingMessage();
         return fetch(url).then(response => {
             return response.json();
         }).then(details => {
@@ -193,14 +237,23 @@ let pokemonRepository = (function (){
             item.imageUrlBack = details.sprites.back_default;
             item.height = details.height;
             item.weight = details.weight;
-            item.types = details.types;
-            item.abilities = details.abilities;
+            item.types = parseTypes(details.types);
+            item.abilities = parseAbilities(details.abilities);
             hideLoadingMessage();
         }).catch(err => {
-            hideLoadingMessage();
+            
             console.log(err);
         });
     }
+
+    function parseAbilities(abilities) {
+        return abilities.map(({ability}) => ability.name).join(',')
+    }
+
+    function parseTypes(types) {
+        return types.map(({type}) => type.name).join(',')
+    }
+    
 
     return {
         add: add, 
